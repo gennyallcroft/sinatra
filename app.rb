8eed0b838266680738e3189020bd4874f75f46d3
Student <student@Makerss-MacBook-Pro.local>
2019-07-08 20:09:01 +0100

    add the form for the cats name

--- app.rb
+++ app.rb
@@ -15,7 +15,13 @@ get '/flowers' do
   'A lovely bouquet of roses'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
